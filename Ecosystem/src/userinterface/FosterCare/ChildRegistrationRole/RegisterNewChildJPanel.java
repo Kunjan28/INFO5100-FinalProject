c0d722d462c5d89a2a27ddc1eaf96c3a7cdd523f
joshi-pa <joshi.pa@northeastern.edu>
2021-04-09 13:54:21 -0400

    UI - Foster Care and Orgs

--- Ecosystem/src/userinterface/FosterCare/ChildRegistrationRole/RegisterNewChildJPanel.java
+++ Ecosystem/src/userinterface/FosterCare/ChildRegistrationRole/RegisterNewChildJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package userinterface.Orphanage.ChildRegistrationRole;
+package userinterface.FosterCare.ChildRegistrationRole;
 
 import Business.Child.Child;
 import Business.Child.ChildDirectory;
